b688ce1dedb4f47e3b2db5422f41e4653c5e1cfd
Pavel Sokolov <48109279+paulsok@users.noreply.github.com>
2023-09-13 23:55:31 +0200

    fix: fix with pep8

--- iterator_final.py
+++ iterator_final.py
@@ -1,22 +1,23 @@
 def count_to(count):
 	"""Our iterator implementation"""
 	
-	#Our list
+	# Our list
 	numbers_in_german = ["eins", "zwei", "drei", "vier", "funf"]
 
-	#Our built-in iterator
-	#Creates a tuple such as (1, "eins")
+	# Our built-in iterator
+	# Creates a tuple such as (1, "eins")
 	iterator = zip(range(count), numbers_in_german)
 	
-	#Iterate through our iterable list
-	#Extract the German numbers
-	#Put them in a generator called number
+	# Iterate through our iterable list
+	# Extract the German numbers
+	# Put them in a generator called number
 	for position, number in iterator:
 		
-		#Returns a 'generator' containing numbers in German
+		# Returns a 'generator' containing numbers in German
 		yield number 
 
-#Let's test the generator returned by our iterator
+
+# Let's test the generator returned by our iterator
 for num in count_to(3):
 	print("{}".format(num))
 
--- visitor_final.py
+++ visitor_final.py
@@ -1,13 +1,15 @@
-class House(object): #The class being visited 
+class House(object):  # The class being visited
 	def accept(self, visitor):
 		"""Interface to accept a visitor"""
-		visitor.visit(self) #Triggers the visiting operation!
+		visitor.visit(self)  # Triggers the visiting operation!
 
 	def work_on_hvac(self, hvac_specialist):
-		print(self, "worked on by", hvac_specialist) #Note that we now have a reference to the HVAC specialist object in the house object!
+		print(self, "worked on by", hvac_specialist)
+	# Note that we now have a reference to the HVAC specialist object in the house object!
 
 	def work_on_electricity(self, electrician):
-		print(self, "worked on by", electrician) #Note that we now have a reference to the electrician object in the house object!
+		print(self, "worked on by", electrician)
+	# Note that we now have a reference to the electrician object in the house object!
 
 	def __str__(self):
 		"""Simply return the class name when the House object is printed"""
@@ -21,29 +23,28 @@ class Visitor(object):
 		return self.__class__.__name__
 
 
-class HvacSpecialist(Visitor): #Inherits from the parent class, Visitor
+class HvacSpecialist(Visitor):  # Inherits from the parent class, Visitor
 	"""Concrete visitor: HVAC specialist"""
 	def visit(self, house):
-		house.work_on_hvac(self) #Note that the visitor now has a reference to the house object
+		house.work_on_hvac(self)  # Note that the visitor now has a reference to the house object
 
 
-class Electrician(Visitor): #Inherits from the parent class, Visitor
+class Electrician(Visitor):  # Inherits from the parent class, Visitor
 	"""Concrete visitor: electrician"""
 	def visit(self, house):
-		house.work_on_electricity(self) #Note that the visitor now has a reference to the house object
+		house.work_on_electricity(self)  # Note that the visitor now has a reference to the house object
 
-#Create an HVAC specialist
+
+# Create an HVAC specialist
 hv = HvacSpecialist()
-#Create an electrician
+# Create an electrician
 e = Electrician()
 
-#Create a house
+# Create a house
 home = House()
 
-#Let the house accept the HVAC specialist and work on the house by invoking the visit() method
+# Let the house accept the HVAC specialist and work on the house by invoking the visit() method
 home.accept(hv)
 
-#Let the house accept the electrician and work on the house by invoking the visit() method
+# Let the house accept the electrician and work on the house by invoking the visit() method
 home.accept(e)
-
-
